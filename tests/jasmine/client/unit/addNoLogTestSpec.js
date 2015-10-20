6c1d2db60cb294fecfe07e73cfad9a22fac29ac1
italvmar <it.alvmar@gmail.com>
2015-10-20 12:07:12 +0200

    add new test check add if not logged in

---
+++ tests/jasmine/client/unit/addNoLogTestSpec.js
@@ -0,0 +1,34 @@
+describe('Meteor methods', function() {
+
+  describe('createNewList', function() {
+
+    beforeEach(function(){
+      spyOn(Lists, "insert");
+    });
+
+    it("No añade lista si no regstrado", function(done) {
+      var listName = "";
+      var err = "not logged-in";
+      
+      Meteor.call("createNewList", listName, function(error, result){
+        expect(error.error).toBe(err);
+        expect(Lists.insert).not.toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it("sholdn't add a new list if the name is not a string", function(done){
+      var listName = 47;
+      var err = "Match failed";
+
+      Meteor.call("createNewList", listName, function(error, result){
+        expect(error.reason).toBe(err);
+        expect(Lists.insert).not.toHaveBeenCalled();
+        done();
+      });
+    });
+
+
+  });
+
+});
